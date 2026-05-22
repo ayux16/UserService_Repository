de7de96ffef5af4e72d747e032d5f6d979071d8a
Ayush <ayushx16@gmail.com>
2026-05-22 18:17:12 +0530

    Spring Security changes Service Discovery

--- src/main/java/org/ecomerce/userservice_repository/Config/ApplicationConfiguration.java
+++ src/main/java/org/ecomerce/userservice_repository/Config/ApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package org.ecomerce.userservice_repository.Config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ApplicationConfiguration {
 
     @Bean
+    @LoadBalanced
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
